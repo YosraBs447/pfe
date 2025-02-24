275a5776525e94963e419139a003cf7590181b89
Eya-B <eyaaaboughzelaa27@gmail.com>
2025-02-24 13:20:03 +0100

    dossier fichier app.js

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import bcryot from 'bcrypt';
+
+const app=express();// create express app
+const port=3000();// port to listen on
+
+cont users=[];// create a users array ya3ni a list of users
+
+app.user
