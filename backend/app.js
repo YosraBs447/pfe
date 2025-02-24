57f5132408cabc6c98590d70e71b74b5878b1694
YosraBs447 <yosrabensaadlg@gmail.com>
2025-02-24 14:06:26 +0100

    first commit

--- backend/app.js
+++ backend/app.js
@@ -7,3 +7,19 @@ const port=3000();// port to listen on
 cont users=[];// create a users array ya3ni a list of users
 
 app.user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.post("/login", async (req, res) => {
